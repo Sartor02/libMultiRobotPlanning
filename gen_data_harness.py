b339bf9b4b6ee5797326fa06d5bb71564590cb2a
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-03-23 17:43:59 -0400

    Added smaller set of requirements

--- gen_data_harness.py
+++ gen_data_harness.py
@@ -13,9 +13,9 @@ kTimeout = 30
 kNumTrials = 500
 kNumIterations = 3
 
-wh_data = range(100, 501, 100)
+wh_data = [100]
 agents_data = range(10, 201, 10)
-density_data = np.linspace(0.01, 0.3, 15)
+density_data = [0.01, 0.1, 0.2, 0.3]
 
 total_iterations = len(wh_data) * len(agents_data) * len(density_data)
 
@@ -27,7 +27,7 @@ iter = 0
 for wh in wh_data:
     for agents in agents_data:
         for density in density_data:
-            f = open("gen_data_harness.result", 'w+')
+            f = open("gen_data_harness.result", 'a')
             f.write("Percent: " + str((iter / total_iterations) * 100) + " Agents: " + str(agents) + " Width/Height: " + str(wh) + " Density: " + str(density) + '\n')
             f.close()
             subprocess.call("../collect_data.py {} {} {} {} {} {} {}".format(agents, wh, wh, density, kNumIterations, kNumTrials, kTimeout), shell=True)
