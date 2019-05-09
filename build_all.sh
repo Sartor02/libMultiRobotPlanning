be9162be6fd7fe7ea94a80299e3b858ad9639eb2
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-05-08 20:38:39 -0400

    Replaced with build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir build;
+mkdir release;
+cd build;
+cmake ..;
+make clean
+make -j`nproc`;
+cd ..;
+cd release;
+cmake -DCMAKE_BUILD_TYPE=Release ..;
+make clean;
+make -j`nproc`;
+cd ..;
+cd afs/AnytimeMAPF;
+make executable;
+cd ../../;
+git submodule update --recursive --init;
+cd mstar_public/cpp;
+make clean;
+make -j`nproc`;
+cd ../../;
