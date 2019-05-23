cfe54ba8184f4f75b219aefaea2e3ab5c4dceb18
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-05-23 00:32:36 -0400

    Added loop script

---
+++ loop_agents.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+agents=("10" "20" "30" "40" "50" "60" "70")
+for i in "${agents[@]}"; do
+    ./xstarstarted;
+    echo "Agents: $i"
+    ./experiments_compare_runtime_data.py "$i" 100 100 0.1 30 1200 60;
+    ./xstardone;
+done
