49cf2e7e40c475f02e60825026ec4d380fdc059b
Kyle Vedder <kyle.c.vedder@gmail.com>
2019-05-23 00:33:51 -0400

    Added missing .sh

--- loop_agents.sh
+++ loop_agents.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 agents=("10" "20" "30" "40" "50" "60" "70")
 for i in "${agents[@]}"; do
-    ./xstarstarted;
+    ./xstarstarted.sh;
     echo "Agents: $i"
     ./experiments_compare_runtime_data.py "$i" 100 100 0.1 30 1200 60;
-    ./xstardone;
+    ./xstardone.sh;
 done
