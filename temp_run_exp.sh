4ac71a18a47b9c25c7014e7d2d3751fde1f52aba
amrl-user <iyerarnav@gmail.com>
2021-10-26 10:33:13 -0500

    datasave no pad

--- temp_run_exp.sh
+++ temp_run_exp.sh
@@ -1,3 +1,7 @@
+python3 experiments_compare_runtime_data.py 10 100 100 .05 30 10 16
+python3 experiments_compare_runtime_data.py 20 100 100 .05 30 10 16
+python3 experiments_compare_runtime_data.py 30 100 100 .05 30 10 16
+python3 experiments_compare_runtime_data.py 40 100 100 .05 30 10 16
 python3 experiments_compare_runtime_data.py 50 100 100 .05 30 10 16
 python3 experiments_compare_runtime_data.py 60 100 100 .05 30 10 16
 
